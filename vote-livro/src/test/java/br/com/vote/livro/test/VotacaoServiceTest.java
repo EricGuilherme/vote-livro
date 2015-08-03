5c8e22ae6e412bbb23e63140ed1e0f3bb8f94e35
Eric Guilherme <eric.guilherme@bluesoft.com.br>
2015-08-03 19:37:29 -0300

    Testes

--- vote-livro/src/test/java/br/com/vote/livro/test/VotacaoServiceTest.java
+++ vote-livro/src/test/java/br/com/vote/livro/test/VotacaoServiceTest.java
@@ -29,9 +29,9 @@ public class VotacaoServiceTest {
 	@Test
 	public void deveComputarVoto(){
 		Livro livro = new Livro(123);
-		
 		Votacao votacao = new Votacao(1);
 		votacao.setLivro(livro);
+		
 		for (int voto = 0; voto <= 5; voto++) {
 			votacaoService.incluirVoto(livro.getLivroKey(), 1);
 			votacao.setVoto(voto);;
@@ -43,6 +43,49 @@ public class VotacaoServiceTest {
 		assertEquals(livro, votacao.getLivro());
 		assertEquals(5, votacao.getVoto());
 	}
+	
+	@Test
+	public void deveSalvarVotacao(){
+		Livro livro = new Livro(123);
+		Votacao votacao = new Votacao(1);
+		votacao.setLivro(livro);
+		
+		votacaoService.salvaVotacao(votacao);
+		
+		
+	}
+	
+	
+
+//    @Test
+//    public void deveSalvarUmBico() {
+//        doReturn(bicoCombustivel).when(bicoCombustivelDao).salvar(bicoCombustivel);
+//
+//        BicoCombustivel bicoCombustivelSalvar = service.salvarBicoCombustivel(bicoCombustivel);
+//
+//        verify(bicoCombustivelDao, times(1)).salvar(bicoCombustivel);
+//        assertEquals(bicoCombustivel, bicoCombustivelSalvar);
+//    }
+//
+//    @Test
+//    public void deveObterUmBicoCombustivel() {
+//        doReturn(bicoCombustivel).when(bicoCombustivelDao).obter(bicoCombustivelKey);
+//
+//        BicoCombustivel bicoCombustivelObter = service.obterBicoCombustivel(bicoCombustivelKey);
+//
+//        verify(bicoCombustivelDao, times(1)).obter(bicoCombustivelKey);
+//        assertEquals(bicoCombustivel, bicoCombustivelObter);
+//    }
+//
+//    @Test
+//    public void deveListarBicoCombustivelPor() {
+//        int bombaCombustivelKey = 10;
+//        service.listarBicoCombustivelPor(bombaCombustivelKey);
+//
+//        verify(bicoCombustivelDao, atLeastOnce()).listarBicoCombustivelPor(bombaCombustivelKey);
+//
+//    }
+
 	
 	
 }
