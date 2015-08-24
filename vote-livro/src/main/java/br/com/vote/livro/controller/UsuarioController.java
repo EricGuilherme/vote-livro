a183d33b84c83c0d23d417a6ae345f602701f2df
Eric Guilherme <eric.guilherme@bluesoft.com.br>
2015-08-24 19:14:34 -0300

    UsuarioControlller

---
+++ vote-livro/src/main/java/br/com/vote/livro/controller/UsuarioController.java
@@ -0,0 +1,30 @@
+package br.com.vote.livro.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.vote.livro.model.Usuario;
+import br.com.vote.livro.service.UsuarioService;
+
+@Controller
+@RequestMapping("usuario/*")
+public class UsuarioController {
+	
+	@Autowired
+	UsuarioService usuarioService;
+	
+	@RequestMapping
+	public String index(){
+		return "usuario/usuario.index.jsp";
+	}
+	
+	@RequestMapping("salvar/")
+	public String salva(String email, String senha){
+		Usuario usuario = new Usuario(email, senha);
+		usuario.setUsuarioKey(1);
+		usuarioService.salvar(usuario);		
+		return "usuario/ranking.usuario.jsp";
+	}
+
+}
